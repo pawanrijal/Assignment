a42ee32ce9a6ac4a1edfeba51b36ff2bcff73e2e
Pawan Rijal <59087554+pawanrijal@users.noreply.github.com>
2020-10-07 21:58:35 +0545

    Update assign.py

--- assign.py
+++ assign.py
@@ -1,8 +1,8 @@
-def is_sorted(stuff):
+def is_sort(stuff):
     for i in range(len(stuff)):
         if stuff[i] > stuff[i-1]:
             return True
     else:
         return False
 numbers = [5,4,3,2,1]
-print(is_sorted(numbers))
+print(is_sort(numbers))
